859669613d362a84b0f59b92c09be0d4d35fbbc1
Ahindra D <official.ahindra@gmail.com>
2022-07-04 20:33:34 +0530

    init

---
+++ src/components/Bars.jsx
@@ -0,0 +1,16 @@
+function Bars(props) {
+    return (
+        <main className="container">
+            <button className="bar c1">C</button>
+            <button className="bar d">D</button>
+            <button className="bar e">E</button>
+            <button className="bar f">F</button>
+            <button className="bar g">G</button>
+            <button className="bar a">A</button>
+            <button className="bar b">B</button>
+            <button className="bar c2">C</button>
+        </main>
+    )
+}
+
+export default Bars;
