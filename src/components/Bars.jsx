fbac6bacaa9663cab9caa6c5cbc219083deb851d
Ahindra D <official.ahindra@gmail.com>
2022-07-04 22:08:29 +0530

    complete

--- src/components/Bars.jsx
+++ src/components/Bars.jsx
@@ -1,14 +1,67 @@
+import React from "react";
+
+
 function Bars(props) {
+    const start1 = () => {
+        const audio1 = new Audio(
+            "./sounds/bvng.mp3"
+        );
+        audio1.play();
+    };
+    const start2 = () => {
+        const audio2 = new Audio(
+            "./sounds/crfo.mp3"
+        );
+        audio2.play();
+    };
+    const start3 = () => {
+        const audio3 = new Audio(
+            "./sounds/dtse.mp3"
+        );
+        audio3.play();
+    };
+    const start4 = () => {
+        const audio4 = new Audio(
+            "./sounds/edqt.mp3"
+        );
+        audio4.play();
+    };
+    const start5 = () => {
+        const audio5 = new Audio(
+            "./sounds/ldcc.mp3"
+        );
+        audio5.play();
+    };
+    const start6 = () => {
+        const audio6 = new Audio(
+            "./sounds/peoi.mp3"
+        );
+        audio6.play();
+    };
+    const start7 = () => {
+        const audio7 = new Audio(
+            "./sounds/tena.mp3"
+        );
+        audio7.play();
+    };
+    const start8 = () => {
+        const audio8 = new Audio(
+            "./sounds/udgb.mp3"
+        );
+        audio8.play();
+    };
+
+
     return (
         <main className="container">
-            <button className="bar c1">C</button>
-            <button className="bar d">D</button>
-            <button className="bar e">E</button>
-            <button className="bar f">F</button>
-            <button className="bar g">G</button>
-            <button className="bar a">A</button>
-            <button className="bar b">B</button>
-            <button className="bar c2">C</button>
+            <button className="bar c1" onMouseMove={start1}>C</button>
+            <button className="bar d" onMouseMove={start2}>D</button>
+            <button className="bar e" onMouseMove={start3}>E</button>
+            <button className="bar f" onMouseMove={start4}>F</button>
+            <button className="bar g" onMouseMove={start5}>G</button>
+            <button className="bar a" onMouseMove={start6}>A</button>
+            <button className="bar b" onMouseMove={start7}>B</button>
+            <button className="bar c2" onMouseMove={start8}>C</button>
         </main>
     )
 }
